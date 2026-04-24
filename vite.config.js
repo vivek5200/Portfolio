8f071dc6feee8814ac73f0186e6848518bedd532
Vivek Bendre <bendrevivek0@gmail.com>
2026-04-24 18:01:44 +0530

    Add vite-plugin-sitemap to enhance sitemap generation

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  plugins: [
+    // ... your other plugins
+    Sitemap({ hostname: 'https://vivekbendre.me' }),
+  ],
+}
