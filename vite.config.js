e6ffe5d8a3cb5db8db7b61f27f585ed6199376c3
Vivek Bendre <bendrevivek0@gmail.com>
2026-04-24 18:06:29 +0530

    Refactor Vite configuration to use defineConfig and improve plugin structure

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import Sitemap from 'vite-plugin-sitemap'
 
-export default {
-  plugins: [
-    // ... your other plugins
-    Sitemap({ hostname: 'https://vivekbendre.me' }),
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react() ,
+    Sitemap({ hostname: 'https://vivekbendre.me' })
   ],
-}
+})
